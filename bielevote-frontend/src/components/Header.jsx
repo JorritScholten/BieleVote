4ff9a74c56812f32e01fddb3a77304bb49789a1e
Jorrit Scholten <jorrit.scholten@hotmail.com>
2023-09-05 14:26:40 +0200

    added remaining libraries to frontend

---
+++ bielevote-frontend/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import PropTypes from "prop-types";
+import { useEffect } from "react";
+
+export default function Header({ pageTitle }) {
+  // sets page title in browser
+  useEffect(() => (document.title = pageTitle), []);
+
+  return (
+    <div>
+      <h1>{pageTitle}</h1>
+    </div>
+  );
+}
+
+Header.propTypes = {
+  pageTitle: PropTypes.string.isRequired,
+};
--- bielevote-frontend/src/page/home/HomePage.jsx
+++ bielevote-frontend/src/page/home/HomePage.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
+import Header from "../../components/Header";
 
 export default function HomePage() {
   const [count, setCount] = useState(0);
-  
+
   return (
     <div>
-      <h1>Vite + React</h1>
+      <Header pageTitle="Home page" />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
