33bf6fcb705000d105a173071d6f26771fbb26ee
David <davidmeijer94@gmail.com>
2023-09-07 13:41:49 +0200

    Created a project proposal page with a form

--- bielevote-frontend/src/App.jsx
+++ bielevote-frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./page/home";
+import ProjectWritingPage from "./components/ProjectWritingPage";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="*" element={<h1>Not Found</h1>} />
+          <Route path="projectwritingpage" element={<ProjectWritingPage />} />
         </Routes>
       </BrowserRouter>
     </>
