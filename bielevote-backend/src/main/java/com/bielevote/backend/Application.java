abcab4773113684b84599c8b670dba6dcb3817dd
David <davidmeijer94@gmail.com>
2023-09-07 11:39:18 +0200

    disabled security config for development

--- bielevote-backend/src/main/java/com/bielevote/backend/Application.java
+++ bielevote-backend/src/main/java/com/bielevote/backend/Application.java
@@ -2,12 +2,13 @@ package com.bielevote.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
