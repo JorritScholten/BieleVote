a9af9b441927db01cd76c8e91edaebb41e362ff8
Jorrit Scholten <jorrit.scholten@hotmail.com>
2023-09-07 12:08:22 +0200

    functioning input form

--- bielevote-frontend/src/page/login/components/LoginForm.jsx
+++ bielevote-frontend/src/page/login/components/LoginForm.jsx
@@ -1,17 +1,13 @@
 import { useState } from "react";
 import { loginForm } from "../../../misc/ApiForms";
-import { Form } from "react-router-dom";
-import { Button } from "semantic-ui-react";
-// import { backendApi } from "../../../misc/ApiMappings";
+import { Form, Button } from "semantic-ui-react";
+import { backendApi } from "../../../misc/ApiMappings";
 
 export default function LoginForm() {
-  const [formData, setFormData] = useState({
-    username: "",
-    password: "",
-  });
+  const [formData, setFormData] = useState(loginForm.emptyForm);
 
-  const handleFormChange = (e, { name, value }) => {
-    if (e.target.inputMode === "numeric") {
+  const handleFormChange = (e, { inputMode, name, value }) => {
+    if (inputMode === "numeric") {
       value = Number.parseInt(value);
       value = value.isNaN ? 0 : value;
     }
@@ -23,50 +19,12 @@ export default function LoginForm() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("handling submit");
-    // backendApi.login(formData);
+    backendApi.login(formData);
+    setFormData(loginForm.emptyForm);
   };
 
   return (
-    <>
-      {/* <Form onSubmit={handleSubmit}>
-        <Form.Input
-          name="username"
-          placeholder="Username"
-          value={formData.username}
-          //   onChange={(e) => handleFormChange(e, setFormData)}
-          pattern="[\S]+"
-          required={true}
-        />
-        <Form.Input
-          name="password"
-          placeholder="Password"
-          value={formData.password}
-          // onChange={(e) => handleFormChange(e, setFormData)}
-          pattern="[\S]+"
-          required={true}
-        />
-        <Button type="submit" content="Login" />
-      </Form> */}
-      {/* <form onSubmit={handleSubmit}>
-        <input
-          name="username"
-          placeholder="Username"
-          value={formData.username}
-          onChange={handleFormChange}
-          pattern="[\S]+"
-          required
-        />
-        <input
-          name="password"
-          placeholder="Password"
-          value={formData.password}
-          onChange={handleFormChange}
-          pattern="[\S]+"
-          required
-        />
-        <button type="submit">Login</button>
-      </form> */}
+    <div>
       <Form onSubmit={handleSubmit}>
         <Form.Input
           name="username"
@@ -86,6 +44,6 @@ export default function LoginForm() {
         />
         <Button type="submit" content="Login" />
       </Form>
-    </>
+    </div>
   );
 }
