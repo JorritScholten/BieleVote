467cc082841fd98d718e0fb5a38427211f0f45d4
Jorrit Scholten <jorrit.scholten@hotmail.com>
2023-09-11 15:58:46 +0200

    created GET mapping for a frontend user to get their account data
    Mapping is used by the (untested) PrintUserInfo frontend component.

--- bielevote-backend/src/main/java/com/bielevote/backend/user/UserController.java
+++ bielevote-backend/src/main/java/com/bielevote/backend/user/UserController.java
@@ -1,9 +1,17 @@
 package com.bielevote.backend.user;
 
+import com.bielevote.backend.authentication.CustomUserDetails;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.NoSuchElementException;
 
 @RequiredArgsConstructor
 @RestController
@@ -11,4 +19,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final UserRepository userRepository;
 
+    @Secured({"citizen:read"})
+    @GetMapping("/me")
+    public ResponseEntity<String> getCurrentUser(@AuthenticationPrincipal CustomUserDetails currentUser) {
+        try {
+            var user = userRepository.findByUsername(currentUser.getUsername()).orElseThrow();
+            return ResponseEntity.ok(JsonMapper.builder()
+                    .build()
+                    .writerWithDefaultPrettyPrinter()
+                    .writeValueAsString(user));
+        } catch (JsonProcessingException e) {
+            return ResponseEntity.internalServerError().body("Couldn't perform serialization.");
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
