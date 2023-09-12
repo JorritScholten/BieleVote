5f0aa5aede7ce0b854299e82dae2ebf4014d0ae7
Jorrit Scholten <jorrit.scholten@hotmail.com>
2023-09-12 15:07:26 +0200

    added logout button to PrintUserInfo

--- bielevote-frontend/src/components/PrintUserInfo.jsx
+++ bielevote-frontend/src/components/PrintUserInfo.jsx
@@ -4,15 +4,18 @@ import { useAuth } from "../misc/AuthContext";
 
 export default function PrintUserInfo() {
   const Auth = useAuth();
+  const { userLogout } = useAuth();
   const [user, setUser] = useState(null);
+
+  function logout() {
+    userLogout();
+    setUser(null);
+  }
 
   async function fetchData() {
     try {
-      console.log("user: " + Auth.getUsername());
       const response = await backendApi.userInfo(Auth.getUser());
-      console.log(response);
       setUser(response.data);
-      console.log(response.data);
     } catch (error) {
       handleLogError(error);
     }
@@ -20,10 +23,13 @@ export default function PrintUserInfo() {
 
   return (
     <div className="bg-slate-200 w-fit h-fit flex flex-col gap-2 p-2">
+      <button className="bg-red-300 p-1" onClick={logout}>
+        logout
+      </button>
       <button className="bg-green-300 p-1" onClick={fetchData}>
         get user info
       </button>
-      <div className="p-1">username: {JSON.stringify(user)}</div>
+      <div className="p-1">info: {JSON.stringify(user)}</div>
     </div>
   );
 }
