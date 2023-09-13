b45235de9643855d1618815651c8fcf805a587ec
Devon Pouw <devon_pouw@hotmail.com>
2023-09-13 15:40:20 +0200

    optimized mappings to return ResponseEntity

--- bielevote-backend/src/main/java/com/bielevote/backend/news/NewsArticleController.java
+++ bielevote-backend/src/main/java/com/bielevote/backend/news/NewsArticleController.java
@@ -8,10 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.awt.print.Pageable;
 import java.time.LocalDateTime;
 import java.util.*;
-import java.util.stream.Collectors;
 
 @RestController
 @CrossOrigin
@@ -21,49 +19,39 @@ public class NewsArticleController {
     @Autowired
     NewsArticleRepository newsArticleRepository;
 
-//    @GetMapping
-//    public List<NewsArticle> getAllArticles() {
-//        return newsArticleRepository.findAll();
-//    }
-
     @GetMapping("/{id}")
-    public Optional<NewsArticle> getArticleById(@PathVariable("id") long id) {
-        return newsArticleRepository.findById(id);
+    public ResponseEntity<NewsArticle> getArticleById(@PathVariable("id") long id) {
+        try {
+            return new ResponseEntity<>(newsArticleRepository.findById(id).orElseThrow(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public NewsArticle postArticle(@RequestBody NewsArticle newsArticle) {
-        if (newsArticle.getDatePlaced() == null) {
-            newsArticle.setDatePlaced(LocalDateTime.now());
+    public ResponseEntity<NewsArticle> postArticle(@RequestBody NewsArticle newsArticle) {
+        try {
+            if (newsArticle.getDatePlaced() == null) {
+                newsArticle.setDatePlaced(LocalDateTime.now());
+            }
+            return new ResponseEntity<>(newsArticleRepository.save(newsArticle), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        return newsArticleRepository.save(newsArticle);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteArticleById(@PathVariable("id") long id) {
-        newsArticleRepository.deleteById(id);
-    }
-
-    record NewsArticlePreviewDto(long id, String title, LocalDateTime datePlaced, String summaryPreview) {
-        public static NewsArticlePreviewDto from(NewsArticle newsArticle) {
-            String[] previewWords = newsArticle.getSummary().split(" ");
-            var preview = String.join(" ", Arrays.copyOf(previewWords, 20)) + "...";
-            return new NewsArticlePreviewDto(newsArticle.getId(), newsArticle.getTitle(), newsArticle.getDatePlaced(), preview);
+    public ResponseEntity<Void> deleteArticleById(@PathVariable("id") long id) {
+        try {
+            newsArticleRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-    }
-
-    @GetMapping
-    public List<NewsArticlePreviewDto> getArticlePreviews() {
-        List<NewsArticle> newsArticles = newsArticleRepository.findAll();
-        return newsArticles.stream()
-                .map(NewsArticlePreviewDto::from)
-                .toList();
     }
 
     @GetMapping("/pages")
     public ResponseEntity<Map<String, Object>> getAllArticles(
-            @RequestParam(required = false) String title,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size
     ) {
@@ -75,17 +63,25 @@ public class NewsArticleController {
 
             newsArticles = pageNewsPre.getContent();
 
-            Map<String, Object> response = new HashMap<>();
-            response.put("articles", new ArrayList<>((newsArticles).stream()
+            Map<String, Object> responseBody = new HashMap<>();
+            responseBody.put("articles", new ArrayList<>((newsArticles).stream()
                     .map(NewsArticlePreviewDto::from)
                     .toList()));
-            response.put("currentPage", pageNewsPre.getNumber());
-            response.put("totalItems", pageNewsPre.getTotalElements());
-            response.put("totalPages", pageNewsPre.getTotalPages());
+            responseBody.put("currentPage", pageNewsPre.getNumber());
+            responseBody.put("totalItems", pageNewsPre.getTotalElements());
+            responseBody.put("totalPages", pageNewsPre.getTotalPages());
 
-            return new ResponseEntity<>(response, HttpStatus.OK);
+            return new ResponseEntity<>(responseBody, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    record NewsArticlePreviewDto(long id, String title, LocalDateTime datePlaced, String summaryPreview) {
+        public static NewsArticlePreviewDto from(NewsArticle newsArticle) {
+            String[] previewWords = newsArticle.getSummary().split(" ");
+            var preview = String.join(" ", Arrays.copyOf(previewWords, 20)) + "...";
+            return new NewsArticlePreviewDto(newsArticle.getId(), newsArticle.getTitle(), newsArticle.getDatePlaced(), preview);
         }
     }
 }
