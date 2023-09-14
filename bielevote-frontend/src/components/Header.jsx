fd92f42a28ae634e0229db4aadb4ae831d5ba960
Jorrit Scholten <jorrit.scholten@hotmail.com>
2023-09-14 14:01:56 +0200

    added NavBar as popup menu to header, implemented hiding based on account type

--- bielevote-frontend/src/components/Header.jsx
+++ bielevote-frontend/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 import LoginForm from "./LoginForm";
 import { Link } from "react-router-dom";
 import { useAuth } from "../misc/AuthContext";
-import { Button } from "semantic-ui-react";
+import { Button, Icon, Popup } from "semantic-ui-react";
 import NavBar from "./NavBar";
 
 export default function Header({ pageTitle }) {
@@ -15,10 +15,21 @@ export default function Header({ pageTitle }) {
 
   return (
     <div className="p-2 gap-2 flex justify-between items-center bg-slate-300">
-      <NavBar />
-      <Link to={"/"} className="w-1/5 text-center text-4xl font-bold">
-        BieleVote
-      </Link>
+      <div className="w-1/5 text-center flex flex-row ">
+        <Popup
+          content={<NavBar />}
+          on="click"
+          popper={{ id: "popper-container", style: { zIndex: 2000 } }}
+          trigger={
+            <Button>
+              <Icon name="content" />
+            </Button>
+          }
+        />
+        <Link to={"/"} className="flex-auto text-4xl font-bold">
+          BieleVote
+        </Link>
+      </div>
       <h1 className="w-1/5 text-center text-3xl">{pageTitle}</h1>
       <div className="w-1/4 self-center">
         {userIsAuthenticated() ? (
--- bielevote-frontend/src/components/NavBar.jsx
+++ bielevote-frontend/src/components/NavBar.jsx
@@ -4,12 +4,15 @@ import { useAuth } from "../misc/AuthContext";
 import { Link } from "react-router-dom";
 
 export default function NavBar() {
-  const { userIsAuthenticated, getAccountType } = useAuth();
+  const { getAccountType } = useAuth();
 
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-2 ">
       {navList
         .filter((nav) => !nav.hideFromMenu)
+        .filter((nav) =>
+          nav.allowedAccountTypes.some((type) => type === getAccountType())
+        )
         .map((nav) => (
           <Link to={nav.path} key={nav.path}>
             {nav.name}
