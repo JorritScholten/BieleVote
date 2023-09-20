ac8d0e449c66968330a1db98af7a55f1d2bed5c5
Jorrit Scholten <jorrit.scholten@hotmail.com>
2023-09-20 19:56:22 +0200

    added a constraint to Vote entity so that one user can only vote for each project once

--- bielevote-backend/src/main/java/com/bielevote/backend/votes/Vote.java
+++ bielevote-backend/src/main/java/com/bielevote/backend/votes/Vote.java
@@ -3,7 +3,6 @@ package com.bielevote.backend.votes;
 import com.bielevote.backend.project.Project;
 import com.bielevote.backend.user.User;
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.extern.jackson.Jacksonized;
@@ -15,27 +14,34 @@ import java.time.LocalDateTime;
 @Setter
 @RequiredArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Jacksonized
-@Table(name = "votes")
+@Table(name = "votes", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"user_id", "project_id"})
+})
 public class Vote {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private Long id;
 
+    @NonNull
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     @JsonBackReference
     private User user;
 
+    @NonNull
     @ManyToOne
     @JoinColumn(name = "project_id", nullable = false)
     @JsonBackReference
     private Project project;
 
+    @NonNull
     @Enumerated(EnumType.STRING)
     private VoteType type;
 
+    @NonNull
     @Column(columnDefinition = "TIMESTAMP(0)")
     private LocalDateTime date;
 }
