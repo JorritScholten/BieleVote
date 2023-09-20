29c425f8206ba4f3af03a4f01b9aaf67d82d8ccd
David <davidmeijer94@gmail.com>
2023-09-20 15:44:37 +0200

    formatted AccountSettingsPage.jsx

--- bielevote-frontend/src/page/accountSettings/AccountSettingsPage.jsx
+++ bielevote-frontend/src/page/accountSettings/AccountSettingsPage.jsx
@@ -10,8 +10,8 @@ import { Header } from "../../components";
 export default function AccountSettingsPage() {
   const [user, setUser] = useState(emptyForms.user);
   const [balance, setBalance] = useState(NaN);
-
   const { getUser } = useAuth();
+
   useEffect(() => {
     async function getBalance() {
       try {
@@ -37,26 +37,26 @@ export default function AccountSettingsPage() {
     <div className="flex flex-col gap-2 w-screen">
       <Header pageTitle="Account settings" />
       <div className="flex justify-center text-3xl w-3/4 mt-12">
-      <List>
-        <List.Item>
-          <List.Icon name="address card" />
-          <List.Content>{user.legalName}</List.Content>
-        </List.Item>
-        <List.Item>
-          <List.Icon name="users" />
-          <List.Content>Username: {user.username}</List.Content>
-        </List.Item>
-        <List.Item>
-          <List.Icon name="phone" />
-          <List.Content>Telephone number: {user.phone}</List.Content>
-        </List.Item>
-        <List.Item>
-          <List.Icon>
-            <GiAcorn />
-          </List.Icon>
-          <List.Content>Balance: {balance}</List.Content>
-        </List.Item>
-      </List>
+        <List>
+          <List.Item>
+            <List.Icon name="address card" />
+            <List.Content>{user.legalName}</List.Content>
+          </List.Item>
+          <List.Item>
+            <List.Icon name="users" />
+            <List.Content>Username: {user.username}</List.Content>
+          </List.Item>
+          <List.Item>
+            <List.Icon name="phone" />
+            <List.Content>Telephone number: {user.phone}</List.Content>
+          </List.Item>
+          <List.Item>
+            <List.Icon>
+              <GiAcorn />
+            </List.Icon>
+            <List.Content>Balance: {balance}</List.Content>
+          </List.Item>
+        </List>
       </div>
     </div>
   );
