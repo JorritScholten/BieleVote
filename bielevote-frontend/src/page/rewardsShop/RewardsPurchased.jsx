45daeff55b9df97fad16faa7d756d128a896a7f1
Devon Pouw <devon_pouw@hotmail.com>
2023-09-25 15:27:08 +0200

    RewardsPurchased now rendered in the front-end

--- bielevote-frontend/src/page/rewardsShop/RewardsPurchased.jsx
+++ bielevote-frontend/src/page/rewardsShop/RewardsPurchased.jsx
@@ -1,20 +1,52 @@
 import { useEffect, useState } from "react";
 import { backendApi, handleLogError } from "../../misc/ApiMappings";
 import { useAuth } from "../../misc/AuthContext";
+import { Header } from "../../components";
+import { emptyForms } from "../../misc/ApiForms";
+import ListRewardsPurchased from "./components/ListRewardsPurchased";
+import { Pagination } from "semantic-ui-react";
 
 export default function RewardsPurchased() {
   const { getUser, userIsAuthenticated } = useAuth();
-  const [purchases, setPurchases] = useState();
+  const [transactionList, setTransactionList] = useState(
+    emptyForms.rewardTransactions
+  );
+
   useEffect(() => {
-    async function getPurchases() {
-      try {
-        const response = backendApi.getAccountBalance(getUser());
-        setPurchases(response.data);
-        console.log(purchases);
-      } catch (error) {
-        handleLogError(error);
-      }
+    handlePageChange();
+  }, [getUser, userIsAuthenticated]);
+  const handlePageChange = async (event, value) => {
+    let page;
+    if (value) {
+      page = value.activePage - 1;
+    } else {
+      page = 0;
     }
-  });
-  return <div></div>;
+    try {
+      const response = await backendApi.getRewardTransactions(
+        getUser(),
+        page,
+        9
+      );
+      setTransactionList(response.data);
+    } catch (error) {
+      handleLogError(error);
+    }
+  };
+
+  return (
+    <div>
+      <Header pageTitle="Rewards purchased" />
+      <div className="flex flex-row items-center justify-center">
+        <ListRewardsPurchased transactionList={transactionList} />
+      </div>
+      <div className="flex flex-row items-center justify-center">
+        <Pagination
+          defaultActivePage={1}
+          totalPages={transactionList.totalPages}
+          onPageChange={handlePageChange}
+        />
+      </div>
+    </div>
+  );
 }
