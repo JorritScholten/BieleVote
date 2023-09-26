64fb9e38c49902b0b260f520ae6dc175c171bc6d
David <davidmeijer94@gmail.com>
2023-09-26 14:16:00 +0200

    created working scheduled method that handles deadlines

--- bielevote-backend/src/main/java/com/bielevote/backend/project/StatusChecker.java
+++ bielevote-backend/src/main/java/com/bielevote/backend/project/StatusChecker.java
@@ -1,6 +1,7 @@
 package com.bielevote.backend.project;
 
 import com.bielevote.backend.votes.VoteRepository;
+import com.bielevote.backend.votes.VoteType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -18,5 +19,21 @@ public class StatusChecker {
     public void checkCompletedDeadlines() {
         System.out.println(LocalDateTime.now() + " --- Checking projects");
         System.out.flush();
+
+        var projects = projectRepository.findByStatus(ProjectStatus.ACTIVE);
+        for (Project project : projects) {
+            if (project.getEndOfVoting().isBefore(LocalDateTime.now())) {
+                int votesFor = voteRepository.findByProjectAndType(project, VoteType.POSITIVE).size();
+                int votesAgainst = voteRepository.findByProjectAndType(project, VoteType.AGAINST).size();
+                if (votesFor > votesAgainst) {
+                    project.setStatus(ProjectStatus.ACCEPTED);
+                } else if (votesAgainst > votesFor) {
+                    project.setStatus(ProjectStatus.REJECTED);
+                } else {
+                    project.setStatus(ProjectStatus.REJECTED);
+                }
+                projectRepository.saveAndFlush(project);
+            }
+        }
     }
 }
