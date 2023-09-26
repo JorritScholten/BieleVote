874d7915ba2ac1ee10ed0571843355dc8993dd3f
David <davidmeijer94@gmail.com>
2023-09-26 16:03:38 +0200

    Added feature that shows the balance in the header

--- bielevote-frontend/src/components/Header.jsx
+++ bielevote-frontend/src/components/Header.jsx
@@ -1,17 +1,29 @@
 import PropTypes from "prop-types";
-import { useEffect } from "react";
-import LoginForm from "./LoginForm";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { useAuth } from "../misc/AuthContext";
 import { Button, Icon, Popup } from "semantic-ui-react";
+import { BiBug } from "react-icons/bi";
+
+import { backendApi } from "../misc/ApiMappings";
 import NavBar from "./NavBar";
+import LoginForm from "./LoginForm";
+import { useAuth } from "../misc/AuthContext";
 
 export default function Header({ pageTitle }) {
-  // sets page title in browser
-  useEffect(() => {
-    document.title = pageTitle;
-  }, [pageTitle]);
   const { userIsAuthenticated, getUsername, userLogout } = useAuth();
+  const [balance, setBalance] = useState(NaN);
+  async function getBalance() {
+    try {
+      const res = await backendApi.getAccountBalance(getUsername());
+      setBalance(res.data);
+    } catch (error) {
+      setBalance(NaN);
+    }
+  }
+    // sets page title in browser
+    useEffect(() => {
+      document.title = pageTitle;
+    }, [pageTitle]);
 
   return (
     <div className="p-2 gap-2 flex justify-between items-center bg-slate-300">
@@ -35,6 +47,7 @@ export default function Header({ pageTitle }) {
         {userIsAuthenticated() ? (
           <Button.Group fluid size="small">
             <Button active className="w-3/4" content={getUsername()} />
+            <Button>{balance}<BiBug></BiBug></Button>
             <Button negative onClick={() => userLogout()} content="Logout" />
           </Button.Group>
         ) : (
