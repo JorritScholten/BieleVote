c8c58648a95deebaa303ac1489cabf371a8ee5e3
Devon Pouw <devon_pouw@hotmail.com>
2023-09-26 13:12:37 +0200

    Created preview components for news and projects

--- bielevote-frontend/src/page/home/HomePage.jsx
+++ bielevote-frontend/src/page/home/HomePage.jsx
@@ -1,11 +1,16 @@
-import { Header } from "../../components";
 import { TestBackendConnection } from "../../components/testing";
+import ProjectsPreview from "./components/ProjectsPreview";
+import NewsPreview from "./components/NewsPreview";
+import { Divider, Grid, HeaderContent, Segment } from "semantic-ui-react";
+import { Header } from "../../components";
 
 export default function HomePage() {
   return (
     <div className="flex flex-col gap-5 w-screen">
       <Header pageTitle="Home page" />
       <TestBackendConnection />
+      <ProjectsPreview />
+      <NewsPreview />
     </div>
   );
 }
