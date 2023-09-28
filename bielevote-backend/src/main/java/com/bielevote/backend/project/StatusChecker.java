e0581418dc58ab84c42c97bf240e7e0524b4e166
Jorrit Scholten <jorrit.scholten@hotmail.com>
2023-09-28 15:08:26 +0200

    @RequiredArgsConstructor interferes with @Value

--- bielevote-backend/src/main/java/com/bielevote/backend/project/StatusChecker.java
+++ bielevote-backend/src/main/java/com/bielevote/backend/project/StatusChecker.java
@@ -5,7 +5,7 @@ import com.bielevote.backend.user.rewardpoint.TransactionReason;
 import com.bielevote.backend.user.rewardpoint.TransactionRepository;
 import com.bielevote.backend.votes.VoteRepository;
 import com.bielevote.backend.votes.VoteType;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -14,14 +14,19 @@ import java.time.LocalDateTime;
 import java.util.concurrent.TimeUnit;
 
 @Component
-@RequiredArgsConstructor
 public class StatusChecker {
-    private final ProjectRepository projectRepository;
-    private final VoteRepository voteRepository;
-    private final TransactionRepository transactionRepository;
+    @Autowired
+    @Value("${app.reward-rules.amount-for-accepted}")
+    int rewardForAccepted;
+    @Autowired
+    private ProjectRepository projectRepository;
+    @Autowired
+    private VoteRepository voteRepository;
+    @Autowired
+    private TransactionRepository transactionRepository;
 
     @Scheduled(fixedRate = 10, timeUnit = TimeUnit.SECONDS)
-    public void checkCompletedDeadlines(@Value("${app.reward-rules.amount-for-accepted}") final int rewardForAccepted) {
+    public void checkCompletedDeadlines() {
         var projects = projectRepository.findByStatusAndEndOfVotingBefore(ProjectStatus.ACTIVE, LocalDateTime.now());
         if (!projects.isEmpty()) {
             System.out.println(LocalDateTime.now() + " --- " + projects.size() + " project(s) have met their deadline.");
